2bb33a0f3805542654f1c771f1d50965f64c9ab3
graceFor <tbgusrud12@gmail.com>
2020-06-29 11:07:06 +0900

    Lecture 1

---
+++ Lecture 1/point.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char c = 'a';
+    char *p = NULL;
+
+    p = &c;
+    printf("%x\n", &c);
+    printf("%x\n", p);
+    printf("%c\n", *p);
+
+    return 0;
+}
