bce6d99a882b1c73f45db39095d09aebeec98da4
graceFor <tbgusrud12@gmail.com>
2020-07-02 13:37:41 +0900

    Lecture 4

---
+++ Lecture 4/stack_local_print.c
@@ -0,0 +1,58 @@
+//stack을 local 변수를 이용해서 만들기
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct item
+{
+    int score;
+    struct item *next;
+};
+
+void insert(struct item **s, int n)
+{
+    struct item *pAlloc;
+    pAlloc = (struct item *)malloc(sizeof(struct item));
+    pAlloc->score = n;
+    pAlloc->next = *s;
+    *s = pAlloc;
+}
+
+int top(struct item *s)
+{
+    return s->score;
+}
+void pop(struct item **s)
+{
+    struct item *pFree;
+    pFree = *s; //삭제할 item 가르키기
+    *s = (*s)->next;
+    free(pFree);
+}
+
+void print_stack(struct item *pS)
+{
+    printf("TOP: ");
+    for (; pS; pS = pS->next)
+    {
+        printf("%d --> ", pS->score);
+    }
+    printf("END\n");
+}
+int main()
+{
+    struct item *pStack = NULL; // stack pointer
+    insert(&pStack, 1);
+    insert(&pStack, 2);
+    insert(&pStack, 3);
+
+    print_stack(pStack);
+    pop(&pStack);
+    print_stack(pStack);
+    pop(&pStack);
+    print_stack(pStack);
+    pop(&pStack);
+    print_stack(pStack);
+
+    return 0;
+}
