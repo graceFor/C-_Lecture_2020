56f7f2f2414f2c4f0cebcf11673200fca2f148e5
graceFor <tbgusrud12@gmail.com>
2020-07-02 13:21:03 +0900

    Lecture 4

---
+++ Lecture 4/stack_global.c
@@ -0,0 +1,50 @@
+//stack을 전역변수를 이용해서 만들기
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct item
+{
+    int score;
+    struct item *next;
+};
+struct item *pStack; // stack pointer
+
+void insert(int n)
+{
+    struct item *pAlloc;
+    pAlloc = (struct item *)malloc(sizeof(struct item));
+    pAlloc->score = n;
+    pAlloc->next = pStack;
+    pStack = pAlloc;
+}
+
+int top()
+{
+    return pStack->score;
+}
+void pop()
+{
+    struct item *pFree;
+    pFree = pStack; //삭제할 item 가르키기
+    pStack = pStack->next;
+    free(pFree);
+}
+
+int main()
+{
+    pStack = NULL;
+    insert(1);
+    insert(2);
+    insert(3);
+
+    printf("%d\n", top()); // 3
+    pop();
+    printf("%d\n", top()); // 2
+    pop();
+    printf("%d\n", top()); // 1
+    pop();
+    printf("%d\n", top());
+
+    return 0;
+}
