3f5ff297a059ad4ee0bc5f68cf0d115dc1e085ef
graceFor <tbgusrud12@gmail.com>
2020-07-03 11:44:15 +0900

    Lecture 5

---
+++ Lecture 5/typedefi.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+typedef int DBLFUN(int, int);
+
+int main()
+{
+    DBLFUN *ff();
+    DBLFUN *fun;
+
+    fun = ff();
+    printf("%d\n", fun(2, 3));
+
+    return 0;
+}
+
+int plus(int x, int y) { return x + y; }
+DBLFUN *ff() { return &plus; }
