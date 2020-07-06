cedf4597dcc2553fcdb07ba98ad522827b6b1bb5
graceFor <tbgusrud12@gmail.com>
2020-07-06 11:07:57 +0900

    Lecture 7

--- Lecture 7/complex_function.c
+++ Lecture 7/complex_function.c
@@ -1,27 +1,29 @@
 // C언어로 객체 만들기 2
 
 #include <stdio.h>
+#include <stdlib.h>
+
 typedef struct
 {
     double re;
     double im;
-    void (*f)(Complex);
+    void (*printfunc)(struct Complex *);
 } Complex;
 
-void print_c(Complex a)
+void print_c(Complex *a)
 {
-    printf("%2.1f+%2.1fi\n", a.re, a.im);
+    printf("%2.1f+%2.1fi\n", a->re, a->im);
 }
 
 int main()
 {
 
-    Complex x = {1.1, 2.2}, y = {3.3, 4.4};
-
-    x.f = print_c;
-    x.f(x);
-    y.f = print_c;
-    y.f(y);
+    Complex *cp;
+    cp = (Complex *)malloc(sizeof(Complex));
+    cp->printfunc = print_c;
+    cp->re = 1.1;
+    cp->im = 2.2;
+    cp->printfunc(cp);
 
     return 0;
 }
