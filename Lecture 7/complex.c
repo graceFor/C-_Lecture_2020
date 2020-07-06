b7a68332f02722a51a0749d9ccb457c54cf2f23c
graceFor <tbgusrud12@gmail.com>
2020-07-06 10:33:38 +0900

    Lecture 7

---
+++ Lecture 7/complex.c
@@ -0,0 +1,23 @@
+// C언어로 객체 만들기
+
+#include <stdio.h>
+typedef struct
+{
+    double re;
+    double im;
+} Complex;
+
+void print_c(Complex a)
+{
+    printf("%2.1f+%2.1f i\n", a.re, a.im);
+}
+
+int main()
+{
+    Complex x = {1.1, 2.2}, y = {3.3, 4.4};
+
+    print_c(x);
+    print_c(y);
+
+    return 0;
+}
