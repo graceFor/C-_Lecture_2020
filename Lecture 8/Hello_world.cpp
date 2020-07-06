4468e92fbdcc9d5e18a90744115dd87bf252943e
graceFor <tbgusrud12@gmail.com>
2020-07-06 12:17:45 +0900

    Lecture 8

---
+++ Lecture 8/Hello_world.cpp
@@ -0,0 +1,15 @@
+// Hello World 출력
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    int number;
+    cout << "정수 1개를 입력하시오.";
+    cin >> number;
+    printf("Hello world\n");
+    cout << number << "\n";
+    return 0;
+}
