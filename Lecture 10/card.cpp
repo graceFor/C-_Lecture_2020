d8cd1a0cde50b21516ef31cab3f29219496a12fc
graceFor <tbgusrud12@gmail.com>
2020-07-07 13:04:23 +0900

    Lecture 10

---
+++ Lecture 10/card.cpp
@@ -0,0 +1,30 @@
+
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+class Card
+{
+public:
+    static int num_instance;
+    int m_data;
+    Card()
+    {
+        num_instance++;
+    }
+    void show()
+    {
+        cout << "일련번호 " << num_instance << endl;
+    }
+};
+int Card::num_instance = 0;
+
+int main()
+{
+    Card c1;
+    c1.show();
+    Card c2;
+    c2.show();
+
+    return 0;
+}
