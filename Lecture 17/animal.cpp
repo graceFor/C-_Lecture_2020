b32949c1e3e96a4cc8d6b29cc380ffab09b5799a
graceFor <tbgusrud12@gmail.com>
2020-07-14 10:27:36 +0900

    Lecture 17

---
+++ Lecture 17/animal.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Animal
+{
+public:
+    Animal(string n) : a_name(n) {}
+    void show() { cout << "Animal " << a_name << endl; }
+
+protected:
+    string a_name;
+};
+
+class Dog : public Animal
+{
+public:
+    Dog(string n) : Animal(n) {}
+};
+
+int main()
+{
+    Animal *a = new Animal("동물");
+    Dog *d = new Dog("개");
+    a = d;
+    a->show();
+    d->show();
+    return 0;
+}
