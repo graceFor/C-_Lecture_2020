e602cf50171ef604b771a4300fd63e2adb530091
graceFor <tbgusrud12@gmail.com>
2020-07-16 10:44:38 +0900

    Lecture 20

---
+++ Lecture 20/INT.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+class Magnitude
+{
+public:
+    virtual int operator<=(Magnitude &x)
+    {
+        cout << "M <= \n";
+        return (*this < x) || (*this == x);
+    }
+    virtual int operator>=(Magnitude &x)
+    {
+        cout << "M >= \n";
+        return !(*this < x);
+    }
+    virtual int operator<(Magnitude &x)
+    {
+        cout << "M < \n";
+        return (*this <= x) && !(*this == x);
+    }
+    virtual int operator>(Magnitude &x)
+    {
+        cout << "M > \n";
+        return (x < *this);
+    }
+    virtual int operator==(Magnitude &x)
+    {
+        cout << "M == \n";
+        return (*this == x);
+    }
+};
+
+class INT : public Magnitude
+{
+public:
+    int data;
+    INT(int a) : data(a) {}
+    int operator==(Magnitude &x)
+    {
+        cout << "Iop == \n";
+        return ((INT *)(&x))->data == this->data;
+    }
+    int operator<(Magnitude &x)
+    {
+        cout << "Iop < \n";
+        return ((INT *)(&x))->data > this->data;
+    }
+};
+
+int main()
+{
+    Magnitude *m1 = new INT(1);
+    Magnitude *m2 = new INT(2);
+    cout << ((*m1) <= (*m2)) << endl;
+    cout << ((*m1) >= (*m2)) << endl;
+    INT a(1), b(2);
+    if (a <= b)
+        cout << "a smaller\n";
+    if (b >= a)
+        cout << "b >= a\n";
+    if (b > a)
+        cout << "b is larger\n";
+    if (a < b)
+        cout << "a is smaler\n";
+}
